9d47a602380cc5c6c65f4a52893f1cffb7b71813
Elliott Short <elliott.short@gmail.com>
2018-07-26 13:36:37 -0500

    Initial commit.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -20,12 +20,19 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
+
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      'ChromeHeadless': {
+        base: 'Chrome',
+        flags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']
+      }
+    },
     singleRun: false
   });
 };
